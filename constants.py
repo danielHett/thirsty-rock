814ba3a2c5c6fe40ee06e148b9194538b1124937
Daniel Hettinger <danielhettinger916@gmail.com>
2024-10-06 13:01:53 -0500

    Write code for setting up IAM resources

---
+++ constants.py
@@ -0,0 +1,36 @@
+# Policy stuff...
+EC2_POLICY_NAME = 'thirsty-rock-ec2-policy'
+EC2_POLICY_DESCRIPTION = 'A policy used for the role used by the thirsty-rock EC2 servers'
+EC2_POLICY_DOCUMENT = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "RandomPermission",
+            "Effect": "Allow",
+			"Action": [
+				"iam:ListPolicies"
+			],
+            "Resource": "*",
+        }
+    ]
+} 
+
+# Role stuff...
+EC2_ROLE_NAME = 'thirsty-rock-ec2-role'
+EC2_ROLE_DESCRIPTION = 'The role used by the thirsty-rock EC2 servers'
+EC2_ASSUME_ROLE_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+} 
+
+# Security group stuff...
+EC2_SECURITY_GROUP_NAME = 'thirsty-rock-security-group'
---
+++ iam_utils.py
@@ -0,0 +1,134 @@
+import logging
+import boto3
+import json
+import constants
+
+def setup_ec2_role():
+    logging.info('Start setup_ec2_role')
+
+    # First, let's check if there is a policy out there.  
+    ec2_iam_policy = get_ec2_iam_policy()
+
+    # If it doesn't exist, make it!
+    if (ec2_iam_policy is None):
+        logging.info('The policy did not exist')
+        ec2_iam_policy = create_ec2_iam_policy()
+
+    # Now we validate that the policy has all of the necessary permissions.  
+    # For now, this returns True. TODO: Write the code that validates the permissions. 
+    if (not is_valid_ec2_iam_policy(ec2_iam_policy)):
+        logging.info('The policy does not have the correct permissions')
+
+    # At this point, we know there is a policy out there which we can use. 
+    # Now we check if the role exists. If not, we make a new role with the policy attached.  
+    ec2_iam_role = get_ec2_iam_role()
+    
+    # If it doesn't exist, make it!
+    if (ec2_iam_role is None):
+        logging.info('The EC2 role did not exist')
+        ec2_iam_role = create_ec2_iam_role()
+
+    attach_policy(ec2_iam_role, ec2_iam_policy)
+
+def get_ec2_iam_role():
+    logging.info('Starting get_ec2_iam_role')
+
+    client = boto3.client('iam')
+
+    try:
+        roles = client.list_roles()['Roles']
+    except:
+        raise Exception("Encountered an error while calling out to IAM.")
+    
+    for role in roles:
+        logging.debug(role)
+        if role['RoleName'] == constants.EC2_ROLE_NAME:
+            logging.info('A role with a matching name was found')
+            logging.info(role)
+            return role
+    
+    logging.info('No matching role was found')
+    return None
+
+def get_ec2_iam_policy():
+    logging.info('Starting get_ec2_iam_policy')
+
+    client = boto3.client('iam')
+
+    try: 
+        policies = client.list_policies(
+            Scope='Local',
+            OnlyAttached=False,
+        )['Policies']
+    except:
+        raise Exception("Encountered an error while calling out to IAM.")
+    
+    for policy in policies:
+        logging.debug(policy)
+        if policy['PolicyName'] == constants.EC2_POLICY_NAME:
+            logging.info('A policy with a matching name was found')
+            logging.info(policy)            
+            return policy
+        
+    logging.info('No matching policy was found')
+    return None
+
+def create_ec2_iam_policy():
+    logging.info('Starting create_ec2_iam_policy')
+
+    client = boto3.client('iam')
+
+    try:
+        policy = client.create_policy(
+            PolicyName=constants.EC2_POLICY_NAME,
+            PolicyDocument=json.dumps(constants.EC2_POLICY_DOCUMENT),
+            Description=constants.EC2_POLICY_DESCRIPTION,
+        )['Policy']
+    except:
+        raise Exception("Encountered an error while calling out to IAM.")
+    
+    logging.info('The policy that was created: ')
+    logging.info(policy)
+
+    return policy
+
+def is_valid_ec2_iam_policy(policy):
+    logging.info('Starting is_valid_ec2_iam_policy')
+
+    return True
+
+def create_ec2_iam_role():
+    logging.info('Starting create_ec2_iam_role')
+
+    client = boto3.client('iam')
+
+    try:
+        role = client.create_role(
+            RoleName=constants.EC2_ROLE_NAME,
+            AssumeRolePolicyDocument=json.dumps(constants.EC2_ASSUME_ROLE_POLICY),
+            Description=constants.EC2_ROLE_DESCRIPTION
+        )['Role']
+    except:
+        raise Exception("Encountered an error while calling out to IAM.")
+
+    logging.info('The role that was created: ')
+    logging.info(role)
+
+    return role
+
+def attach_policy(role, policy):
+    logging.info('Starting attach_policy with (role, policy)')
+    logging.info(role)
+    logging.info(policy)
+
+    client = boto3.client('iam')
+
+    try:
+        client.attach_role_policy(
+            RoleName=role['RoleName'],
+            PolicyArn=policy['Arn']
+        )
+    except:
+        raise Exception("Encountered an error while calling out to IAM.")
+
+    logging.info('Attach successful')
